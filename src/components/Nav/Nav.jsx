8a6bf0df55ef78264bd35c272a7534a740f7bcb6
Bkeefe901 <bradyjokeefe@gmail.com>
2025-11-13 12:44:44 -0500

    added font and some styling

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,21 +2,26 @@ import style from './Nav.module.css';
 import { Link } from 'react-router-dom';
 
 export default function Nav() {
-    
-    return(
+
+    return (
         <nav className={style.nav}>
-            <Link to={'/'} >
-                <h3>Home</h3>
-            </Link>
-            <Link to={'/projects'} >
-                <h3>Projects</h3>
-            </Link>
-            <Link to={'/blog'} >
-                <h3>Blog</h3>
-            </Link>
-            <Link to={'/contact'} >
-                <h3>Contact</h3>
-            </Link>
+            <div className={style.navDiv}>
+                <Link to={'/'} >
+                    <h3>Home</h3>
+                </Link>
+                <Link to={'/projects'} >
+                    <h3>Projects</h3>
+                </Link>
+            </div>
+            <div className={style.navDiv2}>
+                <Link to={'/blog'} >
+                    <h3>Blog</h3>
+                </Link>
+                <Link to={'/contact'} >
+                    <h3>Contact</h3>
+                </Link>
+            </div>
+
         </nav>
     )
 }
