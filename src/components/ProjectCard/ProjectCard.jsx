d6f76fd0bb4b90695c6973c0517aa77e9e406fb1
Bkeefe901 <bradyjokeefe@gmail.com>
2025-11-24 14:58:27 -0500

    Got framer to animate expansion of cards

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,11 +1,23 @@
 import style from './ProjectCard.module.css';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 export default function ProjectCard({ className, children }) {
+    const [isBig, setIsBig] = useState(false);
+
+    function handleClick(e){
+        e.stopPropagation();
+        setIsBig((prev) => !prev);
+    }
     
     return (
-        <div className={className}>
+        <motion.div 
+            className={className}
+            animate={{ height: isBig ? '70%' : '15%' }}  
+            onClick={handleClick} 
+        >
             {children}
             <iframe src="https://github.com/Bkeefe901" frameborder="0"></iframe>
-        </div>
+        </motion.div>
     )
 }
