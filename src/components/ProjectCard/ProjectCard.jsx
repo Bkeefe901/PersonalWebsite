6ae70cb15ff06c9925c75efe62e7d45415eef773
Bkeefe901 <bradyjokeefe@gmail.com>
2025-11-24 18:08:55 -0500

    Commented out projectcard

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,23 +1,23 @@
-import style from './ProjectCard.module.css';
-import { motion } from 'framer-motion';
-import { useState } from 'react';
+// import style from './ProjectCard.module.css';
+// import { motion } from 'framer-motion';
+// import { useState } from 'react';
 
-export default function ProjectCard({ className, children }) {
-    const [isBig, setIsBig] = useState(false);
+// export default function ProjectCard({ className, children }) {
+//     const [isBig, setIsBig] = useState(false);
 
-    function handleClick(e){
-        e.stopPropagation();
-        setIsBig((prev) => !prev);
-    }
+//     function handleClick(e){
+//         e.stopPropagation();
+//         setIsBig((prev) => !prev);
+//     }
     
-    return (
-        <motion.div 
-            className={className}
-            animate={{ height: isBig ? '70%' : '15%' }}  
-            onClick={handleClick} 
-        >
-            {children}
-            <iframe src="https://github.com/Bkeefe901" frameborder="0"></iframe>
-        </motion.div>
-    )
-}
+//     return (
+//         <motion.div 
+//             className={className}
+//             animate={{ height: isBig ? '70%' : '15%' }}  
+//             onClick={handleClick} 
+//         >
+//             {children}
+//             <iframe src="https://github.com/Bkeefe901" frameborder="0"></iframe>
+//         </motion.div>
+//     )
+// }
