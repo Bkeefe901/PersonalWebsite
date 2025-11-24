0e66dbfb59332f5fb49d6d2fc0e52243a53beeed
Bkeefe901 <bradyjokeefe@gmail.com>
2025-11-24 14:38:52 -0500

    Changed style for project cards

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -5,7 +5,7 @@ export default function ProjectCard({ className, children }) {
     return (
         <div className={className}>
             {children}
-            <iframe src="https://gardentracker.onrender.com/" frameborder="5"></iframe>
+            <iframe src="https://github.com/Bkeefe901" frameborder="0"></iframe>
         </div>
     )
 }
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,4 +1,5 @@
 import style from './Projects.module.css';
+import { motion } from 'framer-motion';
 
 // Components
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
@@ -11,16 +12,17 @@ export default function Projects() {
             <header className={style.header}>
                 <h1>My Projects</h1>
             </header>
-            <ProjectCard className={style.gardenCard}>
-                <h1>Garden Tracker</h1>
-            </ProjectCard>
-            <ProjectCard className={style.todoListCard}>
-                <h1>To Do List</h1>
-            </ProjectCard>
-            <ProjectCard className={style.blackjackCard}>
-                <h1>BlackJack</h1>
-            </ProjectCard>
-            
+            <div className={style.cardContainer}>
+                <ProjectCard className={style.gardenCard}>
+                    <h1>Garden Tracker</h1>
+                </ProjectCard>
+                <ProjectCard className={style.todoListCard}>
+                    <h1>To Do List</h1>
+                </ProjectCard>
+                <ProjectCard className={style.blackjackCard}>
+                    <h1>BlackJack</h1>
+                </ProjectCard>
+            </div>
 
         </div>
     )
