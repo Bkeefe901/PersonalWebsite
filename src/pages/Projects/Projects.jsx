8e5123cf27097d73bddb99a7a63899a6c7d69441
Bkeefe901 <bradyjokeefe@gmail.com>
2025-11-26 11:34:25 -0500

    Added links to projects

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -49,7 +49,7 @@ export default function Projects() {
                         <h1>Garden Tracker</h1>
                         <p>The Garden Tracker is a full stack web application that helps you track and care for your personal garden. It allows you to search through the database of plant information to find care tips on the plants in your garden. You can add plants directly from the search section or manually add plants to your garden dashboard. There it will show info on when you last watered and fertalized them, and give color-coded cues based on requirments of when you need to water or feed them next. It offers full Create, Read, Update and Delete functionality to the database. It was built using the MERN stack (MongoDB, Express, React and Node).</p>
                     </div>
-                    <iframe src="https://superbad.com/" frameborder="0"></iframe>
+                    <iframe src="https://gardentracker.onrender.com/" frameborder="0"></iframe>
                 </motion.div>
                 <motion.div
                     className={style.todoListCard}
@@ -60,7 +60,7 @@ export default function Projects() {
                         <h1>To-Do List</h1>
                         <p>The To Do List is a simple React application, built early on while learning react and the use of hooks such as useState and useReducer. It allows you to Create, Read, Update and Delete Items from a to-do list. It has no backend so the information is not kept on refresh.</p>
                     </div>
-                    <iframe src="https://superbad.com/" frameborder="0"></iframe>
+                    <iframe src="https://bradystodo.netlify.app/" frameborder="0"></iframe>
                 </motion.div>
                 <motion.div
                     className={style.blackjackCard}
@@ -71,7 +71,7 @@ export default function Projects() {
                         <h1>Blackjack</h1>
                         <p>The Blackjack app is a simple react application that connects to The Deck of Cards API to create a simple Blackjack game. It makes use of various hooks to manage state in order to keep track of the players and dealers hands.</p>
                     </div>
-                    <iframe src="https://superbad.com/" frameborder="0"></iframe>
+                    <iframe src="https://blackjackreactapp.netlify.app/" frameborder="0"></iframe>
                 </motion.div>
             </div>
 
